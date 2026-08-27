1cfb127c93bf9b2f05911a5798bfd9a95cfdfd00
aqjaffe <adamqjaffe@gmail.com>
2026-08-26 21:36:21 -0400

    Update __init__.py

--- npeb/__init__.py
+++ npeb/__init__.py
@@ -1,3 +1,3 @@
-from npeb.Grenander import Grenander, local_fdr, support_line
-from npeb.PMixture import PMixture
 from npeb.GLMixture import GLMixture
+from npeb.PMixture import PMixture
+from npeb.NCHGMixture import NCHGMixture
